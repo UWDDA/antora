6b567639fdca811d467de0e39c73b4a81bc08a9f
hannah51499 <hannah51499@yahoo.com>
2021-02-24 10:59:18 -0600

    Update bootscript.sh

--- bootscript.sh
+++ bootscript.sh
@@ -1,23 +1,28 @@
-$ sudo apt update
+sudo apt update
 
-$ sudo apt install nodejs
+sudo apt install nodejs
 
-$ sudo apt install npm
+sudo apt install npm
 
-$ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh | bash
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh | bash
 
-$ export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 
-$ nvm install --lts
+nvm install --lts
 
-$ nodejs -v 
+nodejs -v 
 
-$ npm i -g @antora/cli@2.3 @antora/site-generator-default@2.3
+npm i -g @antora/cli@2.3 @antora/site-generator-default@2.3
 
-$ antora -v
+antora -v
 
-$ sudo apt-get install apache2
+sudo apt-get install apache2
 
-$ git clone https://github.com/UWDDA/antora.git
+git clone https://github.com/UWDDA/antora.git
 
+npm i -g http-server 
+
+http-server build/site -c-1
+
+sudo cp -rp demo-site/build/site/* /var/www/html/
