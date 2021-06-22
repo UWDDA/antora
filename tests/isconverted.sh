7598e278a6ef84fa18185f70e158ac9cc5806e61
kgm78 <staticel78@gmail.com>
2021-06-22 16:38:55 +0000

    Added multiple file checking

--- tests/isconverted.sh
+++ tests/isconverted.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
-
-if [[ -f ../file.pdf ]]
-then
-	if [[ -f ../file.adoc ]]
+declare -i total=0
+declare -i converted=0
+for FILE in ../conversions/pdfs/*
+do
+	total=$((total + 1))
+	if [[ -f ../conversions/adocs/FILE ]]
 	then
-		echo "File successfully converted"
+		echo "PDF converted"
+		converted=$((converted + 1))
 	else
 		echo "File not converted"
 	fi
-else
-	echo "There is no file.pdf"
-fi
+done
+echo "$converted out of $total PDFs converted"
