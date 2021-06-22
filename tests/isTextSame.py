471de313bc92bd1490b74676557ec78563f60111
kgm78 <staticel78@gmail.com>
2021-06-22 18:24:10 +0000

    Updated to support checking multiple files

--- tests/isTextSame.py
+++ tests/isTextSame.py
@@ -1,47 +1,65 @@
 #!/usr/bin/python3
 #---Dependencies---
 #sudo apt install poppler util
-
 import subprocess
+import os
 
-#Run pdftotext command in linux bash
-subprocess.call(['pdftotext', '../file.pdf', 'output'])
+#pathname variables
+path_to_pdf = '../conversions/pdfs/'
+path_to_adoc = '../conversions/adocs/'
+total = 0
+passed = 0
 
-#grab asciidoc
-adoc = open('../file.adoc', 'r')
-adocTxt = adoc.read()
+def formatAdocTxt(adocTxt):
+    #get rid of + in asciidoc
+    adocTxt = adocTxt.replace('+', '')
+    #split string into each line
+    adocLines = adocTxt.split('\n')
+    adocTxt = ''
+    #add lines back together removing weird ones
+    for line in adocLines:
+        if not line.startswith('[#'):
+            if not line.startswith('Page: link:#'):
+                adocTxt += line
+    #Remove line breaks
+    adocTxt = adocTxt.replace('\n', '')
+    #remove spaces
+    adocTxt = adocTxt.replace(' ', '')
+    return adocTxt
 
-#get rid of + in asciidoc
-adocTxt = adocTxt.replace('+', '')
-#split string into each line
-adocLines = adocTxt.split('\n')
-    
-adocTxt = ''
-#add lines back together removing weird ones
-for line in adocLines:
-    if not line.startswith('[#'):
-        if not line.startswith('Page: link:#'):
-            adocTxt += line
-#Remove line breaks
-adocTxt = adocTxt.replace('\n', '')
-#remove spaces
-adocTxt = adocTxt.replace(' ', '')
+def formatPdfTxt(pdfTxt):
+    #remove line breaks
+    pdfTxt = pdfTxt.replace('\n', '')
+    pdfTxt = pdfTxt.replace(' ', '')
+    return pdfTxt
 
-#grab output file
-pdfOutput = open('output', 'r')
-pdfTxt = pdfOutput.read()
-
-#remove line breaks
-pdfTxt = pdfTxt.replace('\n', '')
-pdfTxt = pdfTxt.replace(' ', '')
-
-if adocTxt  in pdfTxt:
-    print("Pass")
-else:
-    print("Fail")
-    diff = set(char for char in adocTxt) - set(char for char in pdfTxt)
-    print('Differing characters: ' + diff)
-
-adoc.close()
-pdfOutput.close()
-subprocess.call(['rm', 'output'])
+for pdf in os.listdir(path_to_pdf):
+    #set pdf file name variable
+    pdfName = os.fsdecode(pdf)
+    #make sure current or previous directory isn't selected
+    if pdfName == '.' or pdfName == '..':
+        continue
+    total += 1
+    #call pdftotext command in bash
+    subprocess.call(['pdftotext', path_to_pdf + pdfName, pdfName + '.output'])
+    #grab output file
+    pdfOutput = open(pdfName + '.output', 'r')
+    pdfTxt = pdfOutput.read()
+    #format pdf text
+    pdfTxt = formatPdfTxt(pdfTxt)
+    #grab asciidoc
+    adoc = open(path_to_adoc + pdfName + '.adoc', 'r')
+    #grab text
+    adocTxt = adoc.read()
+    #format
+    adocTxt = formatAdocTxt(adocTxt)
+    #Perform the test
+    if adocTxt  in pdfTxt:
+        print(pdfName + " Passed")
+        passed += 1
+    else:
+        print(pdfName + " Failed")
+    adoc.close()
+    pdfOutput.close()
+    subprocess.call(['rm', pdfName+'.output'])
+print(str(passed) + ' out of ' + str(total))
