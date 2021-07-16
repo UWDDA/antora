a7ae0ee46c59a0c8bce4af021194e153e6eece84
kgm78 <staticel78@gmail.com>
2021-07-16 16:25:25 +0000

    Removed outdated code

--- setup.sh
+++ setup.sh
@@ -18,17 +18,3 @@ sudo npm i -g @antora/cli@2.3 @antora/site-generator-default@2.3
 sudo npm i -g http-server 
 
 antora -v
-
-sudo apt-get install apache2
-
-cd ~
-
-rm -rf antora 
-
-git clone https://github.com/UWDDA/antora.git 
-
-cd antora 
-
-chmod +x bootscript.sh 
-
-./bootscript.sh 
