4847b0cd4f65ef31f6de1125f340cce57c6826f8
kgm78 <staticel78@gmail.com>
2021-07-21 18:45:28 +0000

    Updated buildImages to use and remove ssh key

--- buildImages.sh
+++ buildImages.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
 cd docker/apache2/
+cp ~/intern-deploy .
 sudo docker build -t apache2 .
+rm intern-deploy
 
 cd ../antora/
+cp ~/intern-deploy .
 sudo docker build -t antora .
+rm intern-deploy .
 
 cd ../conversion/
+cp ~/intern-deploy .
 sudo docker build -t conversion .
+rm intern-deploy
