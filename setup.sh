90f924f55e9b31d87672a6156d5b13f6cb201d1c
kgm78 <staticel78@gmail.com>
2021-07-22 15:26:25 +0000

    Removed nvm installation

--- setup.sh
+++ setup.sh
@@ -5,13 +5,6 @@ apt install nodejs -y
 
 apt install npm -y
 
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh | bash
-
-export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-
-nvm install --lts
-
 nodejs -v 
 
 npm i -g @antora/cli@2.3 @antora/site-generator-default@2.3
@@ -20,4 +13,3 @@ npm i -g http-server
 
 antora -v
 
-apt-get install apache2 -y
