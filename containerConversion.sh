a79978c0dc6320d8f846d0843921098471eadebe
kgm78 <staticel78@gmail.com>
2021-07-22 16:31:11 +0000

    Moved file.pdf directory

--- containerConversion.sh
+++ containerConversion.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #Temporary static file location for temp solution
-sudo docker run -it --name file-conversion -v "$(pwd)"/conversionfiles:/~/conversion/data/ conversion
+sudo docker run -it --name file-conversion -v "$(pwd)"/:/home/ubuntu/conversion/data/ conversion
 sudo docker rm file-conversion
