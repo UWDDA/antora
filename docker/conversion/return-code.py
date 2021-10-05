3cdba59265465e64f06bafe315da1b13b0fb8829
kgm78 <staticel78@gmail.com>
2021-10-05 14:45:52 +0000

    Added error handling for reading except message

--- docker/conversion/return-code.py
+++ docker/conversion/return-code.py
@@ -3,6 +3,10 @@ import subprocess
 
 print('returning')
 def handler(event, context):
+    try:
+        bucketname = event['Records'][0]['s3']['bucket']['name']
+    except:
+        print("Event doesn't have records of the S3 bucket")
     bucketname = event['Records'][0]['s3']['bucket']['name']
     print('converting converted files to ' + bucketname + '/adocs/')
     subprocess.run(["./conversion.sh", bucketname])
